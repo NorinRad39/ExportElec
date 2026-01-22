d4c395a90c200c4a199404cf5eec6675e73d1c1a
NorinRad39 <89634711+NorinRad39@users.noreply.github.com>
2026-01-22 22:08:52 +0100

    nom du document recupéré

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,60 @@
-﻿using System;
+﻿using OutilsTs;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using TopSolid.Kernel.Automating;
+using TopSolid.Cad.Design.Automating;
+using TopSolid.Cad.Drafting.Automating;
+using TopSolid.Cam.NC.Kernel.Automating;
+using TSH = TopSolid.Kernel.Automating.TopSolidHost;
+using TSHD = TopSolid.Cad.Design.Automating.TopSolidDesignHost;
 
 namespace ExportElec
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Logique d'interaction pour App.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Document currentDoc;
+        private StartConnect startConnect;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            startConnect = new StartConnect();
+            startConnect.ConnectionTopsolid();
+            InitializeForm();
+
+        }
+        private void InitializeForm()
+        {
+            // Initialisation de currentDoc
+            currentDoc = new Document();
+            currentDoc.DocId = TSH.Documents.EditedDocument;
+            DocumentNameText.Text = currentDoc.DocNomTxt;
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportStep_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
