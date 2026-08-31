4e963187873153f49fd0f437ab7ad92311dedd45
Florent FABBRI <norinrad.39@gmail.com>
2026-08-31 14:21:52 +0200

    Erreur d'archi pour deploy

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
